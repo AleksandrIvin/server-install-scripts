195dd0f47a06dbd463eea84147271d3e914ddd2a
Aleksandr <aleksandr.ivin.ua@gmail.com>
2018-12-24 14:26:55 +0100

    Changed Compose version

--- docker_compose_install.sh
+++ docker_compose_install.sh
@@ -1,4 +1,4 @@
-VERSION=1.23.1
+VERSION=1.22.0
 sudo curl -L "https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
